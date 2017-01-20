8820b370d822dba28fae4e6509490700bc88848e
jousley <jousley@gmail.com>
2017-01-20 16:48:21 -0600

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,44 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: 'jeff', age: 25};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect o MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('unable to insert todo', err);            
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // })
+
+
+    // db.collection('Users').insertOne({
+    //     name: 'Jeff Ousley',
+    //     age: 47,
+    //     location: 'Kansas City'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('unable to insert user', err);            
+    //     }
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // })
+
+
+    db.close();
+});
